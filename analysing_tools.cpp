26066b85ec035eb62e1eec4c990309e77629b559
Gubarev Semen <mnogono@gmail.com>
2018-01-12 00:57:26 +0500

    update

---
+++ analysing_tools.cpp
@@ -0,0 +1,109 @@
+#include "sound_analysis_config.h"
+
+class AnalysisTools {
+  private:
+    AnalysisTools();
+
+  public:
+
+    static void intToDouble(int *values, size_t size, double **result);
+};
+
+void AnalysisTools::intToDouble(int *values, size_t size, double **result) {
+  *result = new double[size];
+  for (size_t i = 0; i < size; ++i) {
+    (*result)[i] = static_cast<double>(values[i]);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int *vals = new int[10];
+  for (auto i = 0; i < 10; ++i) {
+    vals[i] = i;
+  }
+
+  double *dvals = nullptr;
+  AnalysisTools::intToDouble(vals, 10, &dvals);
+  for (int i = 0; i < 10; ++i) {
+    std::cout << dvals[i] << std::endl;
+  }
+  if (dvals != 0) {
+//    delete []dvals;
+  }
+//  fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
+  return 0;
+}
+
+/*
+package com.envionsoftware.sounds.analysis;
+
+import java.util.Arrays;
+
+public class AnalysingTools {
+    // Disable instantiation:
+    private AnalysingTools() {}
+
+    public static double[] intToDouble(int[] values) {
+        double[] result = new double[values.length];
+        for (int k = 0; k < result.length; k++) {
+            result[k] = (double) values[k];
+        }
+        return result;
+    }
+
+    // Functions below are C-like: they work with first count elements of the passed arrays
+    // and do not try to allocate memory (for maximal speed).
+    static double maxValue(double[] values, int count) {
+        double result = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i < count; i++) {
+            result = Math.max(result, values[i]);
+        }
+        return result;
+    }
+
+    static double percentile(double[] values, int offset, int count, double[] workMemory, double level) {
+        if (count <= 0) {
+            // to be on the safe side
+            return Double.NaN;
+        }
+        //TODO!! use histogram 256 values between min and max (precision will be enough, O(256)+O(count) ops)
+        System.arraycopy(values, offset, workMemory, 0, count);
+        Arrays.sort(workMemory, 0, count);
+        // - the simplest, but not the fastest way
+        int index = (int) Math.round(level * count);
+        if (index > count - 1) {
+            index = count - 1;
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        return workMemory[index];
+    }
+
+    static int averageCount(int count, int aperture) {
+        aperture = Math.max(aperture, 1);
+        if (aperture >= count) {
+            return 0;
+        }
+        return count - aperture + 1;
+    }
+
+    static void average(double[] result, double[] values, int count, int aperture) {
+        aperture = Math.max(aperture, 1);
+        final int resultLen = averageCount(count, aperture);
+        if (resultLen == 0) {
+            return;
+        }
+        double sum = 0.0;
+        for (int k = 0; k < aperture; k++) {
+            sum += values[k];
+        }
+        double apertureInv = 1.0 / aperture;
+        result[0] = sum * apertureInv;
+        for (int i = aperture, k = 1; k < resultLen; i++, k++) {
+            sum += values[i] - values[k - 1];
+            result[k] = sum * apertureInv;
+        }
+    }
+}
+*/
---
+++ sound_analysis_config.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cstddef>
+#include <iostream>
+
+#define SOUND_ANALYSIS_VERSION_MAJOR @SOUND_ANALYSIS_VERSION_MAJOR@
+#define SOUND_ANALYSIS_VERSION_MINOR @SOUND_ANALYSIS_VERSION_MINOR@
